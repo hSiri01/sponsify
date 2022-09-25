beaafab715ea544f99beb7122ec99a838b95932b
racreitien <rachelburns7@gmail.com>
2022-09-25 17:12:10 -0500

    setup connection to mongo DB with mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect(
+    process.env.MONGODB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+
+const db = mongoose.connection;  // access default connection
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
