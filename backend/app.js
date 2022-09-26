2b838a5ef24cba9eb5661d76a940fbdb11cff2c0
racreitien <rachelburns7@gmail.com>
2022-09-25 20:47:18 -0500

    change port num and concurrently command

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = 3001
 
 mongoose.connect(
     process.env.MONGODB_URL,
@@ -16,7 +16,7 @@ const db = mongoose.connection;  // access default connection
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
