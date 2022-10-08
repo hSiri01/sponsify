ca30ebab1b7245d567f6b7a9bb47b2d6435ce1eb
sabrinapenaa <sabrinapena.7721@gmail.com>
2022-10-08 18:09:08 -0500

    Added Level Add + Level Edit features. Need to work on setting color.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Inbox from './sponsor/organism/Inbox/App'
 
 import EditFAQ from './admin/organism/EditFAQ/App';
 import Dashboard from './admin/organism/Dashboard/App'
+import EditLevels from './admin/organism/EditLevels/App';
 
 
 
@@ -36,6 +37,7 @@ const App: React.FC = () => {
             <Route path="/checkout-swe" element={<Checkout student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/inbox-swe" element={<Inbox student_org_logo={SWELogo} />} />
             <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
+            <Route path="/levels-edit-swe" element={<EditLevels student_org_logo={SWELogo} student_org_name="SWE" />} />
           <Route path="/dashboard-swe" element={<Dashboard student_org_logo={SWELogo} sponsor_code="1l2x9gkd/I3GJD!%[
 " valid_until_date={new Date(2022, 10, 14)} student_org_name="Society of Women Engineers" street_address='3127 TAMU' city='College Station' state="TX" zip_code={77843} fund_name="947490-SWE"/>} />
 
