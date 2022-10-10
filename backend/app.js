333d92828662b9b3e54b1bfd780ee903553f0103
sabrinapena <sabrinapena@tamu.edu>
2022-10-09 21:48:55 -0500

    Added Sponsor route. Added 3 Event routes.

--- backend/app.js
+++ backend/app.js
@@ -22,3 +22,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App listening on port ${port} :)`)
 })
+app.get('/create-sponsor', (req,res) => {
+    res.send('Create Sponsor')
+})
+app.get('/update-event', (req,res) => {
+    res.send('Update Event')
+})
+app.get('/delete-event', (req,res) => {
+    res.send('Delete Event')
+})
+app.get('/checkout', (req,res) => {
+    res.send('Checkout')
+})
