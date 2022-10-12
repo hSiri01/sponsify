d7ed11bfd6e5eafbc5b515414b8452c18f9449aa
Anjali Segu <asegu117@tamu.edu>
2022-10-12 10:14:53 -0500

    Minor edits

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,10 @@ const App: React.FC = () => {
             <Route path="/events-swe" element={<Events student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/checkout-swe" element={<Checkout student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/inbox-swe" element={<Inbox student_org_logo={SWELogo} />} />
+            
             <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
-          <Route path="/dashboard-swe" element={<Dashboard student_org_logo={SWELogo} sponsor_code="1l2x9gkd/I3GJD!%[
-" valid_until_date={new Date(2022, 10, 14)} student_org_name="Society of Women Engineers" street_address='3127 TAMU' city='College Station' state="TX" zip_code={77843} fund_name="947490-SWE"/>} />
+            <Route path="/dashboard-swe" element={<Dashboard student_org_logo={SWELogo} sponsor_code="1l2x9gkd/I3GJD!%[
+            " valid_until_date={new Date(2022, 10, 14)} student_org_name="Society of Women Engineers" street_address='3127 TAMU' city='College Station' state="TX" zip_code={77843} fund_name="947490-SWE"/>} />
 
           </Routes>
       </BrowserRouter>
