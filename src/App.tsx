fe0ccb8cca0b9b6443f0253e0db36318c4ba1a92
Anjali Segu <asegu117@tamu.edu>
2022-10-12 20:05:03 -0500

    Fix merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import SponsorHome from './sponsor/organism/Home/App';
 import HowItWorks from './sponsor/organism/HowItWorks/App';
 import FAQ from './sponsor/organism/FAQ/App';
-import EditFAQ from './admin/organism/EditFAQ/App';
 import Levels from './sponsor/organism/Levels/App';
 import Events from './sponsor/organism/Events/App';
 import { ThemeProvider } from '@mui/system';
@@ -12,8 +11,6 @@ import SWELogo from './assets/images/graphics/SWE_logo.png';
 import Checkout from './sponsor/organism/Checkout/App'
 import Inbox from './sponsor/organism/Inbox/App'
 
-<<<<<<< HEAD
-=======
 import EditFAQ from './admin/organism/EditFAQ/App';
 import Dashboard from './admin/organism/Dashboard/App'
 import EditEvents from './admin/organism/EditEvents/App'
@@ -21,7 +18,6 @@ import PurchaseHistory from './admin/organism/PurchaseHistory/App';
 import EditLevels from './admin/organism/EditLevels/App';
 
 
->>>>>>> 62e03c7006c733c3d67507bb9443b26cf3f2cdfe
 
 import {
   BrowserRouter,
@@ -43,17 +39,12 @@ const App: React.FC = () => {
             <Route path="/events-swe" element={<Events student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/checkout-swe" element={<Checkout student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/inbox-swe" element={<Inbox student_org_logo={SWELogo} />} />
-<<<<<<< HEAD
-          <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
-=======
-            
             <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
             <Route path="/levels-edit-swe" element={<EditLevels student_org_logo={SWELogo} student_org_name="SWE"student_org_short_name="swe" />} />
           <Route path="/dashboard-swe" element={<Dashboard student_org_logo={SWELogo} sponsor_code="1l2x9gkd/I3GJD!%[
             " valid_until_date={new Date(2022, 10, 14)} student_org_name="Society of Women Engineers" student_org_short_name="swe" street_address='3127 TAMU' city='College Station' state="TX" zip_code={77843} fund_name="947490-SWE"/>} />
           <Route path="/events-edit-swe" element={<EditEvents student_org_logo={SWELogo} student_org_name="SWE" />} />
           <Route path="/summary-swe" element={<PurchaseHistory student_org_logo={SWELogo} student_org_name="SWE" total_sponsored={50000} />} />
->>>>>>> 62e03c7006c733c3d67507bb9443b26cf3f2cdfe
 
           </Routes>
       </BrowserRouter>
