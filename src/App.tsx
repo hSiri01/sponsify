f39fe21a0c2503d479f0e76d44713d77d7439cd4
monicaarrambide <55818711+monicaarrambide@users.noreply.github.com>
2022-10-12 20:05:42 -0500

    Remove merge conflict comments

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,12 @@ import SWELogo from './assets/images/graphics/SWE_logo.png';
 import Checkout from './sponsor/organism/Checkout/App'
 import Inbox from './sponsor/organism/Inbox/App'
 
-<<<<<<< HEAD
-=======
-import EditFAQ from './admin/organism/EditFAQ/App';
 import Dashboard from './admin/organism/Dashboard/App'
 import EditEvents from './admin/organism/EditEvents/App'
 import PurchaseHistory from './admin/organism/PurchaseHistory/App';
 import EditLevels from './admin/organism/EditLevels/App';
 
 
->>>>>>> 62e03c7006c733c3d67507bb9443b26cf3f2cdfe
 
 import {
   BrowserRouter,
@@ -43,9 +39,6 @@ const App: React.FC = () => {
             <Route path="/events-swe" element={<Events student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/checkout-swe" element={<Checkout student_org_logo={SWELogo} level_color="ebeaea" level_name='Platinum' total={4250}/>} />
             <Route path="/inbox-swe" element={<Inbox student_org_logo={SWELogo} />} />
-<<<<<<< HEAD
-          <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
-=======
             
             <Route path="/faq-edit-swe" element={<EditFAQ student_org_logo={SWELogo} student_org_name="SWE" />} />
             <Route path="/levels-edit-swe" element={<EditLevels student_org_logo={SWELogo} student_org_name="SWE"student_org_short_name="swe" />} />
@@ -53,7 +46,6 @@ const App: React.FC = () => {
             " valid_until_date={new Date(2022, 10, 14)} student_org_name="Society of Women Engineers" student_org_short_name="swe" street_address='3127 TAMU' city='College Station' state="TX" zip_code={77843} fund_name="947490-SWE"/>} />
           <Route path="/events-edit-swe" element={<EditEvents student_org_logo={SWELogo} student_org_name="SWE" />} />
           <Route path="/summary-swe" element={<PurchaseHistory student_org_logo={SWELogo} student_org_name="SWE" total_sponsored={50000} />} />
->>>>>>> 62e03c7006c733c3d67507bb9443b26cf3f2cdfe
 
           </Routes>
       </BrowserRouter>
