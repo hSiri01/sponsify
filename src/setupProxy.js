969cdb6f1a4114f139dcb4eeba63ac53dc45b5de
hs <hsiri01@tamu.edu>
2022-10-13 12:02:21 -0500

    heroku debugging pt4

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,17 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-          target: 'http://localhost:5000',
-          changeOrigin: true,
-        })
-    );
+// module.exports = function(app) {
+//     app.use(
+//         '/api',
+//         createProxyMiddleware({
+//           target: 'http://localhost:5000',
+//           changeOrigin: true,
+//         })
+//     );
+// };
+
+const proxy = require("http-proxy-middleware");
+
+module.exports = app => {
+  app.use(proxy("/api/*", { target: "http://localhost:3001/" }));
 };
