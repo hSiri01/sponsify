6da16cc40951d5ab7b486d2ac60819343bdb12b7
hs <hsiri01@tamu.edu>
2022-10-20 08:14:15 -0500

    working on proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const proxy = require("http-proxy-middleware");
-
-module.exports = app => {
-  app.use(proxy("/api/*", { target: "http://localhost:3001/" }));
+module.exports = function(app) {
+  app.use(proxy("/api/**", { // https://github.com/chimurai/http-proxy-middleware
+    target: "http://localhost:3001",
+    secure: false
+  }));
 };
